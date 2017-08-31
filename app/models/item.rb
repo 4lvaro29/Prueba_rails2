b503f8ef8c67230f0b9f6b35ac530cb787420166
Alvaro Urbina <4lvaro.up@gmail.com>
2017-08-31 12:01:19 -0300

    third

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
+  belongs_to :user, optional: true
+  scope :volumen, ->(x) {where(size: x)}
+  after_save :name, capitalize: true
+  #hacer scopes
 end
