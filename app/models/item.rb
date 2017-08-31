b95314688dc19cc9b7d31543119176942a591dde
Alvaro Urbina <4lvaro.up@gmail.com>
2017-08-31 13:05:46 -0300

    se agrega scaffold y controllers

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :category
   belongs_to :user, optional: true
+
   scope :volumen, ->(x) {where(size: x)}
 
   after_save :user_name, capitalize: true
---
+++ app/views/works/_work.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! work, :id, :user, :item, :category, :created_at, :updated_at
+json.url work_url(work, format: :json)
